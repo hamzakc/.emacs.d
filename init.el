ee8bc7380b713252850cd5da37abcb794530151b
Hamza Khan-Cheema <hamza@khan-cheema.com>
2020-03-22 19:10:27 +0000

    Upgrade configuration to use straight

--- init.el
+++ init.el
@@ -1,34 +1,111 @@
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
+;;;;  Effectively replace use-package with straight-use-package
+;;; https://github.com/raxod502/straight.el/blob/develop/README.md#integration-with-use-package
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
 
-(package-initialize)
+;; Configure emacs via org babel
 (require 'ob-tangle)
-(load "~/.emacs.d/lisp/PG/generic/proof-site")
-(load "~/.emacs.d/private.el")
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
+ '(ansi-color-names-vector
+	 ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
+ '(custom-safe-themes
 	 (quote
-		("~/Documents/org/calendars/kc.org" "~/Documents/org/calendars/noths.org" "~/Documents/org/calendars/ukhols.org" "~/Documents/org/gtd/gtd.org" "~/Documents/org/gtd/inbox.org" "~/Documents/org/gtd/someday.org" "~/Documents/org/gtd/tickler.org")))
+		("7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(fci-rule-color "#073642")
+ '(golden-ratio-mode t)
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+	 (quote
+		("#98695021d64f" "#484f5a50ffff" "#9ae80000c352" "#00000000ffff" "#98695021d64f" "#9ae80000c352" "#484f5a50ffff")))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+	 (quote
+		(("#073642" . 0)
+		 ("#5b7300" . 20)
+		 ("#007d76" . 30)
+		 ("#0061a8" . 50)
+		 ("#866300" . 60)
+		 ("#992700" . 70)
+		 ("#a00559" . 85)
+		 ("#073642" . 100))))
+ '(hl-bg-colors
+	 (quote
+		("#866300" "#992700" "#a7020a" "#a00559" "#243e9b" "#0061a8" "#007d76" "#5b7300")))
+ '(hl-fg-colors
+	 (quote
+		("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+ '(hl-paren-colors (quote ("#2aa198" "#b58900" "#268bd2" "#6c71c4" "#859900")))
+ '(lsp-ui-doc-border "#93a1a1")
+ '(nrepl-message-colors
+	 (quote
+		("#dc322f" "#cb4b16" "#b58900" "#5b7300" "#b3c34d" "#0061a8" "#2aa198" "#d33682" "#6c71c4")))
  '(org-export-backends (quote (ascii html icalendar latex md confluence)))
+ '(org-roam-directory "/home/hamza/Documents/org/")
  '(package-selected-packages
 	 (quote
-		(sudo-ext vagrant-tramp org-gcal org-journal feature-mode calfw calfw-org calfw-ical calfw-cal auto-complete switch-window evil use-package auto-compile)))
+		(:straight emojify-mode emojify browse-at-remote plantuml-mode magit-gh-pulls keychain-environment slim-mode golden-ratio exec-path-from-shell markdown-mode 0blayout flymake-eslint flymake-jslint eglot egot indium ac-slime slime dmenu exwm scala-mode pomidor sudo-ext vagrant-tramp org-gcal org-journal feature-mode calfw calfw-org calfw-ical calfw-cal auto-complete switch-window evil use-package auto-compile)))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+	 (quote
+		((20 . "#dc322f")
+		 (40 . "#ffffa21b0000")
+		 (60 . "#ffffd2170000")
+		 (80 . "#b58900")
+		 (100 . "#fffffffe0000")
+		 (120 . "#fffffffe0000")
+		 (140 . "#fffffffe0000")
+		 (160 . "#fffffffe0000")
+		 (180 . "#859900")
+		 (200 . "#dc61ffb77bfe")
+		 (220 . "#c516ffa79f16")
+		 (240 . "#a726ffaac017")
+		 (260 . "#7bfcffc6e035")
+		 (280 . "#2aa198")
+		 (300 . "#0000fffffffe")
+		 (320 . "#0000fffffffe")
+		 (340 . "#0000fffffffe")
+		 (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+	 (quote
+		(unspecified "#002b36" "#073642" "#a7020a" "#dc322f" "#5b7300" "#859900" "#866300" "#b58900" "#0061a8" "#268bd2" "#a00559" "#d33682" "#007d76" "#2aa198" "#839496" "#657b83")))
+ '(winner-mode t)
+ '(xterm-color-names
+	 ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#eee8d5"])
+ '(xterm-color-names-bright
+	 ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
